affb9d1a6bc651bd8d96ade98b2e3416dd9f81b9
Errafai <errafaiachraf1923@gmail.com>
2024-09-25 18:52:15 +0100

    1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,21 +11,23 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 
-	binary_tree_t *left = binary_tree_node(parent, value);
+	binary_tree_t *left;
 	binary_tree_t *temp;
 
-	if (parent == NULL && left == NULL)
+	if (parent == NULL)
 		return (NULL);
 
-	if (parent->left == NULL)
-		parent->left = left;
-	else
+	left = binary_tree_node(parent, value);
+	if (left == NULL)
+		return (NULL);
+
+	if(parent->left != NULL)
 	{
 		temp = parent->left;
-		parent->left = left;
 		left->left = temp;
 		temp->parent = left;
 	}
+	parent->left = left;
 
 	return (left);
 
