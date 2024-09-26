720a9b961239be4d18a1b739cf40d870bbaaac47
Errafai <errafaiachraf1923@gmail.com>
2024-09-26 23:51:10 +0100

    13-binary_tree_nodes.c

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - calculate the nodes with one child of a binary tree
+ * @tree: the root of the binary tree
+ * Return: the nodes of the tree
+ */
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left != NULL || tree->right != NULL)
+		return binary_tree_nodes(tree->right) +  binary_tree_nodes(tree->left) + 1;
+	return (0);
+
+}
