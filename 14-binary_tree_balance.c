1c006e2d08018905f78ecb6d49e59cdab344fafd
Errafai <errafaiachraf1923@gmail.com>
2024-09-27 16:43:24 +0100

    14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -10,13 +10,13 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t r = 0, l = 0;
 
-	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+	if (tree == NULL)
 		return (0);
 
-	r = binary_tree_height(tree->right);
-	l = binary_tree_height(tree->left);
+	r = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+	l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
 
-	return (1 + (l > r ? l : r));
+	return (l > r ? l : r);
 }
 
 /**
